95ff6b04c6eaab521b30d6fb4b04485dbe151461
Abrar Zaher <abrarzaher@gmail.com>
2019-09-09 13:41:04 -0600

    Update sketch.js

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -5,13 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redamount = 0;
+let redchange = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0, 0, 0);
 }
 
 function draw() {
-  background(220);
-  fill(0);
+  fill(redamount, 0, 210);
   ellipse(mouseX, mouseY, 100, 100);
+
+  redamount += redchange;
+
+  if (redamount >= 255) {
+    redchange *= -1;
+  }
+
+  if (redamount <= 0) {
+    redchange *= -1;
+  }
+
+  console.log(redamount);
 }

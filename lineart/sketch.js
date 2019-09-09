0b9c83fdd31db3e9e1ab03d3624289cbc0362802
Abrar Zaher <abrarzaher@gmail.com>
2019-09-09 14:11:26 -0600

    worked on demo lineart

--- lineart/sketch.js
+++ lineart/sketch.js
@@ -1,31 +1,15 @@
-// Project Title
-// Your Name
+// Line Art Demo
+// Abar
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let redamount = 0;
-let redchange = 1;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0, 0, 0);
+  background(255);
 }
 
 function draw() {
-  fill(redamount, 0, 210);
-  ellipse(mouseX, mouseY, 100, 100);
-
-  redamount += redchange;
-
-  if (redamount >= 255) {
-    redchange *= -1;
-  }
-
-  if (redamount <= 0) {
-    redchange *= -1;
-  }
-
-  console.log(redamount);
+  line(mouseX, mouseY, pmouseX, pmouseY);
 }

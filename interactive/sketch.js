2d526647bc1c925c762afe2e2c515f86bc89f887
bananasarehealthy <51250121+bananasarehealthy@users.noreply.github.com>
2019-09-24 23:58:23 -0600

    interactive

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -5,23 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//input box
+let inputBox;
+
 //states
 let state = 0;
 /*
   0 = intro;
   more to be added later
 */
+let introTextNum = 0;
 
 //backgrounds
 let introBg;
 
 //characters
 let playerName;
-
-let prof1;
-let prof2;
-let prof3;
-let prof4;
+let prof;
 
 //dialog
 let IntroDialog = ["Welcome to the monde de Pokebro!!!", "I am Songru Tom, the world's leading researcher in the field of flexology.", "Oh, I'm sorry but what was your name again???", "Ah yes, hello " + playerName + "!", "Wait... are you a boy or a girl???", "Oh right, my bad, anyways, your journey to becoming the greatest flexer of the century begins now!"];
@@ -29,39 +29,73 @@ let IntroDialog = ["Welcome to the monde de Pokebro!!!", "I am Songru Tom, the w
 //buttons
 let aPressed = false;
 let bPressed = false;
+let inputButton;
 
 function preload() {
   IntroBg = loadImage("assets/introbackground.PNG");
-  prof = [loadImage("assets/professor1.PNG"), loadImage("assets/professor1.PNG"), loadImage("assets/professor2.PNG"), loadImage("assets/professor1.PNG"), loadImage("assets/professor3.PNG", loadImage("assets/professor4.PNG"))];
+  prof = [loadImage("assets/professor1.PNG"), loadImage("assets/professor1.PNG"), loadImage("assets/professor2.PNG"), loadImage("assets/professor1.PNG"), loadImage("assets/professor3.PNG"), loadImage("assets/professor4.PNG")];
 }
 
 function setup() {
   createCanvas(3 * (windowWidth/5), (3 * (windowWidth/5))/1.6);
   background(0);
-  noLoop();
 }
 
 function draw() {
   if (state === 0) {
     gameIntro();
   } 
-  //startGame();
+  else if (state === 1) {
+    background(0);
+  }
 }
 
 
 function gameIntro() {
-  let textNum = 0;
-
   background(IntroBg);
-  image(prof[textNum], width/3, height/6, width/4, 5 * (height/8))
+  image(prof[introTextNum], width/3, height/6, width/4, 5 * (height/8))
   //fadeImageIn(prof, width/3, height/6, width/4, 5 * (height/8));
 
-  textBox(IntroDialog[textNum]); 
+  textBox(IntroDialog[introTextNum]); 
+
+  if (introTextNum === 6) {
+    state = 1;
+  }
+  // else if (introTextNum === 2) {
+  //   pickName();
+  // }
+  else if (introTextNum === 4) {
+    pickSprite();
+  }
 
   if (aPressed) {
-    textNum++;
+    introTextNum++;
     aPressed = false;
   }
+}
+
+function pickName() {
+  inputBox = createInput();
+  inputBox.position(20, 300);
+
+  inputButton = createButton('Submit');
+  inputButton.position(inputBox.x + inputBox.width, 300);
+  inputButton.mousePressed(setPlayerName);
+
+  greeting = createElement('h2', 'What is your name?');
+  greeting.position(20, 245);
+
+  textSize(50);
+}
+
+function setPlayerName() {
+  playerName = inputBox.value();
+
+  inputBox.remove();
+  inputButton.remove();
+  greeting.remove();
+  
+  introTextNum++;
 }
 
 function textBox(theText) {

1d2e021df506453759e3b7431c04aab5d85bc91f
Abrar Zaher <abrarzaher@gmail.com>
2019-09-26 12:23:22 -0600

    yo

--- anotherballbounce/sketch.js
+++ anotherballbounce/sketch.js
@@ -7,48 +7,90 @@
 
 let x;
 let y;
+let dx;
+let dy;
 let radius = 100;
-let dx = 10;
-let dy = 10;
-let mode = 2;
+let rectSize = 100;
+let mode = "menu";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-
+  
   x = width/2;
   y = height/2;
+  dx = random(-15, 15);
+  dy = random(-15, 15);
 }
 
 function draw() {
   background(255);
-  fill(0);
 
-  x += dx;
-  y += dy;
+  moveShape();
 
-  if (mode === 1) {
-    if (x > width - radius/2 || x < 0 + radius/2) {
-      dx *= -1;
-    }
-    if (y > height - radius/2 || y < 0 + radius/2) {
-      dy *= -1;
-    }
-
-    circle(x, y, radius);
+  if (mode === "menu") {
+    showMenu();
+    checkButtonPressed();
   }
-  else if (mode === 2) {
-    if (x > width || x < 0) {
-      dx *= -1;
-    }
-    if (y > height || y < 0) {
-      dy *= -1;
-    }
+  else if (mode === "circle") {
+    displayCircle();
+  }
+  else if (mode === "rectangle") {
+    displayRectangle();
+  }
+}
 
-    rectangle(x, y, radius, radius);
+function showMenu() {
+  rectMode(CENTER);
+  fill(255, 0, 0, 128);
+  rect(width/2, height/2 - 150, 400, 150);
+  rect(width/2, height/2 + 150, 400, 150);
+  textAlign(CENTER, CENTER)
+  textSize(50);
+  fill(0);
+  text("Rectangle", width/2, height/2 - 150);
+  text("Circle", width/2, height/2 + 150);
+
+  if (mouseIsPressed) {
+    if (mouseX > (width/2 - 200) && mouseX < width/2 + 200 && mouseY < height/2 - 75 && mouseY > height/2 + 225) {
+      mode = "rectangle";
+    }
   }
 }
 
 function windowResized() {
   setup();
+}
+
+function moveShape() {
+  // move
+  x += dx;
+  y += dy;
+}
+
+function displayCircle() {
+  // bounce if needed
+  if (x > width - radius/2 || x < 0 + radius/2) {
+    dx *= -1;
+  }
+
+  if (y > height - radius/2 || y < 0 + radius/2) {
+    dy *= -1;
+  }
+
+  fill(0);
+  circle(x, y, radius);
+}
+
+function displayRectangle() {
+  // bounce if needed
+  if (x > width - rectSize || x < 0) {
+    dx *= -1;
+  }
+
+  if (y > height - rectSize || y < 0) {
+    dy *= -1;
+  }
+
+  fill(0);
+  rect(x, y, rectSize, rectSize);
 }

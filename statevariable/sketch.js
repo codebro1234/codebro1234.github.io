e0d7e134508884171ecf7bd612b0dfcc782290b1
Abrar Zaher <abrarzaher@gmail.com>
2019-10-01 15:09:41 -0600

    state variable

--- statevariable/sketch.js
+++ statevariable/sketch.js
@@ -5,6 +5,25 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let playerSprite;
+
+class dudes {
+  constructor(theName, theSprite) {
+    this.name = theName;
+    this.sprite = theSprite;
+  }
+
+  display() {
+    Image()
+  }
+}
+
+let mainPlayer = new dudes("Bro", playerSprite);
+
+function preload() {
+  playerSprite = loadImage("assets/")
+}
+
 function setup() {
   createCanvas(3 * (windowWidth/5), (3 * (windowWidth/5))/1.6);
   background(220);

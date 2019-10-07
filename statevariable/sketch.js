cd3690a70e0d7be6fdbf080ef1010839d838dd0e
Abrar Zaher <abrarzaher@gmail.com>
2019-10-07 14:11:18 -0600

    sgfds

--- statevariable/sketch.js
+++ statevariable/sketch.js
@@ -5,6 +5,40 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let mode = 0;
+let backgroundImage;
+
+let mainPlayer;
+
+let directions = {
+  down: 0,
+  up: 1,
+  right: 2,
+  left: 3
+};
+
+let currentDirections = directions.down;
+
+function preload() {
+  playerSprite = loadImage("assets/")
+  backgroundImage = loadImage("assets/")
+}
+
+function setup() {
+  createCanvas(3 * (windowWidth/5), (3 * (windowWidth/5))/1.6);
+  background(220);
+
+  mainPlayer = new Dudes("Bro",  , width/2, height/2);
+}
+
+function draw() {
+  if (mode === 0) {
+    walkAround();
+  }
+
+ //rect(3 * (width/4), height * 0.05, (width/4) * 0.9, height * 0.9); 
+}
+
 class Dudes {
   constructor(theName, sprite1, sprite2, sprite3, sprite4, x, y) {
     this.name = theName;
@@ -21,33 +55,15 @@ class Dudes {
 
   move() {
     if () {
-
+      
     }
   }
 } 
 
-let mainPlayer = new Dudes("Bro",  , width/2, height/2);
+function walkAround() {
+  background(backgroundImage);
 
-let directions = {
-  down: 0,
-  up: 1,
-  right: 2,
-  left: 3
-} ;
-
-let currentDirections = directions.down;
-
-function preload() {
-  playerSprite = loadImage("assets/")
-}
-
-function setup() {
-  createCanvas(3 * (windowWidth/5), (3 * (windowWidth/5))/1.6);
-  background(220);
-}
-
-function draw() {
- rect(3 * (width/4), height * 0.05, (width/4) * 0.9, height * 0.9); 
+  
 }
 
 function keyPressed() {

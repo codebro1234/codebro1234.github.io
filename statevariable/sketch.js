386b652329cfc7976d1482195fc79199f0b849b7
Abrar Zaher <abrarzaher@gmail.com>
2019-10-16 14:11:16 -0600

    bggfdh

--- statevariable/sketch.js
+++ statevariable/sketch.js
@@ -12,6 +12,11 @@ let mainPlayer;
 
 let mainCharacterSprites;
 
+let movingUp = false;
+let movingDown = false;
+let movingRight = false;
+let movingLeft = false;
+
 let directions = {
   down: 0,
   up: 1,
@@ -28,17 +33,17 @@ function preload() {
 
 function setup() {
   createCanvas(3 * (windowWidth/5), (3 * (windowWidth/5))/1.6);
-  background(220);
-
-  mainPlayer = new Dudes("Bro",  , width/2, height/2);
+  background(backgroundImage);
+  
+  mainPlayer = new Dudes("Bro", mainCharacterSprites, width/2, height/2);
 }
 
 function draw() {
   if (mode === 0) {
     walkAround();
   }
-  else if () {
-
+  else if (mode === 1) {
+    background(0);
   }
 }
 
@@ -56,44 +61,71 @@ class Dudes {
     image(this.sprite[currentDirections], this.x, this.y, 100, 100);
   }
   
-  move() {
-    if (currentDirections === 0) {
+  // move() {
+  //   if (movingDown) {
       
-    }
-    else if (currentDirections === 1) {
+  //   }
+  //   else if (movingUp) {
 
-    }
-    else if (currentDirections === 2) {
+  //   }
+  //   else if (movingRight) {
       
-    }
-    else if (currentDirections === 3) {
+  //   }
+  //   else if (movingLeft) {
       
-    }    
-  }
+  //   }    
+  // }
 } 
 
 function walkAround() {
-  background(backgroundImage);
-  
+  mainPlayer.display();
+  mainPlayer.move();
+
+  if (keyPressed) {
+    if (key === " ") {
+      mode++;
+    }
+  }
+}
+
+function displayMenu () {
   stroke(0);
   rect(3 * (width/4), height * 0.05, (width/4) * 0.9, height * 0.9); 
-  stroke(0, )
-  rect(3 * (width/4) + (((width/4) * 0.9) * 0.1), height * 0.05, (width/4) * 0.9, (height * 0.9)/4); 
-  rect(3 * (width/4), height * 0.05, (width/4) * 0.9, height * 0.9); 
-  rect(3 * (width/4), height * 0.05, (width/4) * 0.9, height * 0.9); 
+  for (let i = height * 0.05; i > (height * 0.05) + height * 0.9; i += (height * 0.9)/4) {
+    rect(3 * (width/4) + (((width/4) * 0.9) * 0.1), i, (width/4) * 0.9, (height * 0.9)/4);
+  }   
 }
 
 function keyPressed() {
   if (keyCode === DOWN_ARROW) {
     currentDirections = directions.down;
+    movingDown = true;
   } 
   else if (keyCode === UP_ARROW) {
     currentDirections = directions.up;
+    movingUp = true;
   }
   else if (keyCode === RIGHT_ARROW) {
     currentDirections = directions.right;
+    movingRight = true;
   }
   else if (keyCode === DOWN_ARROW) {
     currentDirections = directions.left;
+    movingLeft = true;
+  }
+}
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    movingDown = false;
+  } 
+  else if (keyCode === UP_ARROW) {
+    movingUp = false;
+  }
+  else if (keyCode === RIGHT_ARROW) {
+    movingRight = false;
+  }
+  else if (keyCode === DOWN_ARROW) {
+    movingLeft = false;
   }
 }

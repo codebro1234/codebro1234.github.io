1af248f195080856e9155a9edac27dfafeeedd73
Abrar Zaher <abrarzaher@gmail.com>
2019-10-24 14:15:45 -0600

    stfsr

--- grids/sketch.js
+++ grids/sketch.js
@@ -5,45 +5,63 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let grid = [];
+let grid;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  grid = create2DArray(20, 20);
+  createCanvas(500, 500);
+  grid = create2DArray(30, 30);
 }
 
 function draw() {
-  background(255);
-
-  showGrid(grid);
+  background(220);
+  
+  showGrid(grid, 30, 30);
 }
 
-function showGrid(theGrid) {
-  let cellSize = width/theGrid[0]/length;
-
-  for (let i = 0; i < theGrid[0].length; i++) {
-    for (let j = 0; j < theGrid[0].length; j++) {
-      fill(theGrid[i][j] * 255);
-
-      rect(i * cellSize, j * cellSize, cellSize, cellSize);
+function showGrid(someGrid, cols, rows) {
+  for (let j = 0; j < someGrid[0].length; j++) {
+    for (let i = 0; i < someGrid[0].length; i++) {
+      fill((someGrid[i][j]) * 255);
+      rect((width/cols) * i, (height/cols) * j, width/cols, height/rows); 
     }
   }
 }
 
 function create2DArray(cols, rows) {
   let someArray = [];
-
+  
   for (let i = 0; i < cols; i++) {
     someArray.push([]);
     for (let j = 0; j < rows; j++) {
       if (random(100) < 50) {
-        someArray[i].push(1);
-      }
-      else {
         someArray[i].push(0);
+      } else {
+        someArray[i].push(1);
       }
     }
   }
-
+  
   return someArray;
+}
+
+function mousePressed() {
+  let cellSize = width/30;
+  
+  let xCoord = floor(mouseX/cellSize);
+  let yCoord = floor(mouseY/cellSize);
+  
+  grid[xCoord][yCoord] = 0;
+}
+
+function keyTyped() {
+  if (key === 'r') {
+    grid = create2DArray(30, 30);
+  } else if (key === 'c') {
+    for (let j = 0; j < grid[0].length; j++) {
+      for (let i = 0; i < grid[0].length; i++) {
+        grid[i][j] = 1;
+         
+      }
+    }
+  }
 }

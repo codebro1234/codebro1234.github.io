88b0c776e74c0fdcc930a0f259b69cac86672bb1
Abrar Zaher <abrarzaher@gmail.com>
2019-10-28 14:11:01 -0600

    ujghv

--- grids/sketch.js
+++ grids/sketch.js
@@ -1,67 +1,137 @@
-// Line Art Demo
-// Abar
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
 let grid;
+let rows = 30;
+let cols = 30;
 
 function setup() {
-  createCanvas(500, 500);
-  grid = create2DArray(30, 30);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  grid = createRandom2dArray(cols, rows);
 }
 
 function draw() {
   background(220);
-  
-  showGrid(grid, 30, 30);
+  displayGrid(grid, rows, cols);
 }
 
-function showGrid(someGrid, cols, rows) {
-  for (let j = 0; j < someGrid[0].length; j++) {
-    for (let i = 0; i < someGrid[0].length; i++) {
-      fill((someGrid[i][j]) * 255);
-      rect((width/cols) * i, (height/cols) * j, width/cols, height/rows); 
-    }
+function windowResized() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
   }
-}
-
-function create2DArray(cols, rows) {
-  let someArray = [];
-  
-  for (let i = 0; i < cols; i++) {
-    someArray.push([]);
-    for (let j = 0; j < rows; j++) {
-      if (random(100) < 50) {
-        someArray[i].push(0);
-      } else {
-        someArray[i].push(1);
-      }
-    }
+  else {
+    createCanvas(windowWidth, windowWidth);
   }
-  
-  return someArray;
-}
-
-function mousePressed() {
-  let cellSize = width/30;
-  
-  let xCoord = floor(mouseX/cellSize);
-  let yCoord = floor(mouseY/cellSize);
-  
-  grid[xCoord][yCoord] = 0;
 }
 
 function keyTyped() {
-  if (key === 'r') {
-    grid = create2DArray(30, 30);
-  } else if (key === 'c') {
-    for (let j = 0; j < grid[0].length; j++) {
-      for (let i = 0; i < grid[0].length; i++) {
-        grid[i][j] = 1;
-         
+  if (key === "r") {
+    grid = createRandom2dArray(cols, rows);
+  }
+  if (key === "c") {
+    grid = createEmptyGrid();
+  }
+  if (key === " ") {
+    update();
+  }
+}
+
+function mousePressed() {
+  let cellSize = width/cols;
+
+  let xCoord = floor(mouseX / cellSize);
+  let yCoord = floor(mouseY / cellSize);
+  
+  if (grid[yCoord][xCoord] === 1) {
+    grid[yCoord][xCoord] = 0;
+  }
+  else {
+    grid[yCoord][xCoord] = 1;
+  }
+}
+
+function createEmptyGrid() {
+  let emptyGrid = [];
+  for (let x = 0; x < cols; x++) {
+    emptyGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      emptyGrid[x].push(0);
+    }
+  }
+  return emptyGrid;
+}
+
+function update() {
+  let nextTurn = createEmptyGrid();
+
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      let neighbors = 0;
+
+      //loop around the neighbor spots...
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //deal with edge cases
+          if (x+i >= 0 && x+i < cols && y+j >= 0 && y+j < rows) {
+            neighbors += grid[y+j][x+i];
+          }
+        }
+      }
+      //don't count self as a neighbor
+      neighbors -= grid[y][x];
+
+      //apply rules!
+      if (grid[y][x] === 1) { //currently alive
+        if (neighbors === 2 || neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0) { //currently dead
+        if (neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
       }
     }
   }
+  grid = nextTurn;
+}
+
+function displayGrid(grid, rows, cols) {
+  let cellSize = width / cols;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill(255);
+      }
+      else {
+        fill(0);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(1);
+      }
+      else {
+        randomGrid[x].push(0);
+      }
+    }
+  }
+  return randomGrid;
 }

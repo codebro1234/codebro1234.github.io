df114bd5d2cb432838a3cb1e6f1a3cf82c3ba222
bananasarehealthy <51250121+bananasarehealthy@users.noreply.github.com>
2019-11-18 00:20:00 -0600

    started working on battle mechanics for major project

---
+++ battlething/sketch.js
@@ -0,0 +1,29 @@
+// Battle Thing
+// Abar
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  
+  textBox("yo bruv");
+}
+
+// class Bros {
+//   constructor(name,)
+// }
+
+function textBox(theText) {
+  fill(255);
+  rect(width * 0.01, 3 * (height/4) - height * 0.01, width - width * 0.02, height/4, 20);
+  
+  fill(0);
+  textSize(20);
+  text(theText, width * 0.05, 3 * (height/4) + height * 0.03, width - width * 0.05, height/4)
+}
